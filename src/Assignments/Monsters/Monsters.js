8eebea4bae7c34850391a907500cad6de56cb341
Jongmin Choi <quark4904@gmail.com>
2021-12-11 16:17:58 +0900

    Modify: input 태그 마진값 원복

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -42,7 +42,7 @@ function Monsters() {
     <div className="monsters">
       <h1>컴포넌트 재사용 연습!</h1>
       {/* <SearchBox handleChange=정의한메소드 /> */}
-      <input type="text" placeholder="search..." />
+      <SearchBox />
       {/* <CardList monsters=몬스터리스트 /> */}
       <CardList monsters={monsters} />
     </div>
